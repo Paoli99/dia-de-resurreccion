1d51e9dbdad059d8d02febf5a5c5c4e39afdf702
Paola Vilaseca <pao_anahi_8@hotmail.com>
2025-04-13 21:08:30 -0400

    fixed text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Tumba from "./components/Tumba";
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-none flex flex-col items-center justify-center text-center px-6 pt-10">
-      <div className="mb-40">
+      <div className="mb-100">
       <h1 className="text-4xl font-bold text-gray-800" style={{ fontFamily: 'StarJedi' }}>
         Feliz domingo de resurrección
       </h1>
